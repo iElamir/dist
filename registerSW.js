5fc395a4334b321934adef59cab56793fc7c2f3f
Mohamed Elamir <mohammed.elamir@pharmacymarts.com>
2025-05-08 01:07:11 +0300

    alert the error

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' }).catch(e => alert("registerSW.js   "+e.message))})}
--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
+try{
 if(!self.define){let e,i={};const n=(n,r)=>(n=new URL(n+".js",r).href,i[n]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=i,document.head.appendChild(e)}else e=n,importScripts(n),i()})).then((()=>{let e=i[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(r,t)=>{const s=e||("document"in self?document.currentScript.src:"")||location.href;if(i[s])return;let o={};const l=e=>n(e,s),u={module:{uri:s},exports:o,require:l};i[s]=Promise.all(r.map((e=>u[e]||l(e)))).then((e=>(t(...e),o)))}}define(["./workbox-5ffe50d4"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"index.html",revision:null},{url:"placeholder.svg",revision:null},{url:"registerSW.js",revision:null},{url:"manifest.webmanifest",revision:"b96d94b003f7390740c298c22cdf9dc5"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))}));
+} catch (error) {
+    alert("sw.js    "+error.message)    
+}
